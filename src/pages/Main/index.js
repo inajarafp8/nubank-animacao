5223d9676e45948bed446aeb65ff3469e32cc81f
inajarafp8 <inajarafp8@gmail.com>
2019-08-30 21:17:57 -0300

    Tamanho do qrcode

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+
 import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation 
 } from './styles';
 import Header from '~/components/Header';
@@ -8,32 +11,87 @@ import Tabs from '~/components/Tabs';
 import Menu from '~/components/Menu';
 
 export default function Main (){
+  let offset = 0;
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        }
+      }
+    ],
+    { useNativeDriver: true }, 
+  )
+
+  function onHandlerStateChanged(event){
+    if(event.nativeEvent.oldState === State.ACTIVE){
+      let opened = false;
+      const { translationY } = event.nativeEvent;
+
+      offset += translationY;
+
+      if(translationY >= 100){
+        opened = true;
+      }else{
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue:opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
+  }
+
   return (
-    <Container>
+    <Container >
       <Header />
 
       <Content>
-        <Menu />
+        <Menu translateY={translateY} />
         
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="Visibility-off" size={28} color="#666" />
-          </CardHeader>
-          <CardContent>
-            <Title>Saldo disponivel</Title>
-            <Description>R$ 2.000.000.000,00</Description>
-              
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Transferência de R$ 1.000,00 recebida de Kamila Simões Antônio hoje às 12:00h
-            </Annotation>
-          </CardFooter>
-        </Card>
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChanged}
+        >
+          <Card style={{
+            transform: [{
+              translateY: translateY.interpolate({
+                inputRange: [-350, 0, 380],
+                outputRange: [-50, 0, 380],
+                extrapolate: 'clamp',
+              }),
+            }],
+          }}
+          >
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponivel</Title>
+              <Description>R$ 2.000.000.000,00</Description>
+                
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 1.000,00 recebida de Kamila Simões Antônio hoje às 12:00h
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
 
-      <Tabs />
+      <Tabs translateY={translateY} />
     </Container>
   );
 }
